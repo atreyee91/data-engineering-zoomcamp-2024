fa0a8d8e4a6e489d5219bbad082fc9f0885bbe77
atreyee91 <104919070+atreyee91@users.noreply.github.com>
2024-02-18 13:25:23 +0530

    De dbt branch (#3)
    * init my dbt project
    
    * deploy the project
    
    * removed limit
    
    ---------
    
    Co-authored-by: Atreyee Das <rohit.kundu@icloud.com>

--- 04-analytics-engineering/taxi-rides-ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi-rides-ny/models/core/fact_trips.sql
@@ -55,9 +55,3 @@ on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
 on trips_unioned.dropoff_locationid = dropoff_zone.locationid
 
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
